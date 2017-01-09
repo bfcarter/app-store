4daa59fed95b5dc5574696e105c4854118e75169
Brittni Carter <bfcarter@fullsail.edu>
2017-01-08 19:52:15 -0600

    comments added to js

--- controllers/getRandomText.js
+++ controllers/getRandomText.js
@@ -1,14 +1,14 @@
 module.exports = function(length) {
-    if(length < 1) {
-        length = 10;
+    if(length < 1) { //if greater then 1
+        length = 10; //less then 10 in length
     }
 
     var result = "";
-    var allowed = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var allowed = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"; //variables that are allowed
 
     for(var i=0; i<length; i++) {
         result += allowed.charAt(Math.floor(Math.random() * allowed.length));
-    }
+    } 
 
-    return result;
+    return result; //answer
 };
